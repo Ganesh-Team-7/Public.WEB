6639e0de3e155d43ade70c36d80109eff3a2212d
ganeshachari br <ganeshachari.pri@gmail.com>
2025-07-15 19:31:44 +0530

    Included both API and Web in one solution

---
+++ Public.WEB/Controllers/Product/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Public.WEB.Controllers.Product
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
